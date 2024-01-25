309c1d945990016cc1c4e3414e75b1f55ecb3431
Arthur-Melchior <108128616+Arthur-Melchior@users.noreply.github.com>
2024-01-25 09:11:48 +0100

    ajouter test test_multiplier_plusieurs_nombres

--- TDD/tests/test_stringcalculator.py
+++ TDD/tests/test_stringcalculator.py
@@ -30,3 +30,18 @@ def test_add_plusieurs_nombres(mon_param, mon_resultat):
     somme = StringCalculator.add(mon_param)
     # assert
     assert somme == mon_resultat
+
+
+@pytest.mark.parametrize("mon_param_mult, mon_resultat_mult", [
+    ("3;5", 15),
+    ("3;6", 18),
+    ("3;7", 21),
+    ("3;8", 24),
+    ("3;9", 27),
+    ("3;10", 30),
+])
+def test_multiplier_plusieurs_nombres(mon_param_mult, mon_resultat_mult):
+    # act
+    resultat = StringCalculator.multiply(mon_param_mult)
+    # assert
+    assert resultat == mon_resultat_mult
