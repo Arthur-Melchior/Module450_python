b4aed31847c8cd1b589e1905d15fffce2d7f2f61
Arthur-Melchior <108128616+Arthur-Melchior@users.noreply.github.com>
2024-01-27 10:18:14 +0100

    rajouter test fontionel (test_multiply_a_letter_from_a_number) qui va essayer de multiplier un chiffre et une lettre et vice-versa

--- TDD/tests/test_stringcalculator.py
+++ TDD/tests/test_stringcalculator.py
@@ -47,6 +47,17 @@ def test_multiply_multiple_number(my_param_mult, my_result_mult):
     assert result == my_result_mult
 
 
+@pytest.mark.parametrize("my_param_mult2, my_result_mult2", [
+    ("a;5", 'Invalid Input'),  # test case
+    ("18;bonjour", 'Invalid Input'),  # test case 2
+])
+def test_multiply_a_letter_from_a_number(my_param_mult2, my_result_mult2):
+    # act
+    result = StringCalculator.multiply(my_param_mult2)
+    # assert
+    assert result == my_result_mult2
+
+
 @pytest.mark.parametrize("my_param_divide, my_result_divide", [
     ("15;3", 5),  # test case
     ("18;3", 6),  # test case 2
